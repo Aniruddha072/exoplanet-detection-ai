22f04a27e8b058a32258a41f5ea905af0a4f870c
Devi Chatterjee <229901595+mayavichatterjee1710@users.noreply.github.com>
2026-06-29 00:02:07 +0530

    Data for false positives

---
+++ Data inspection1.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+# loading the local csv file
+base_dir = Path(__file__).resolve().parent.parent
+train_data = pd.read_csv(base_dir / "data" / "exoTrain.csv")
+test_data = pd.read_csv(base_dir / "data" / "exoTest.csv")
+
+# Check the dimensions of the matrix
+
+print(f"Train data shape: {train_data.shape}")
+print(f"Test data shape: {test_data.shape}")
+
+# Extract features and labels from the training set
+X_train = train_data.drop(columns="LABEL").values
+y_train = train_data["LABEL"].values
+
+# Find the index of the first non-planet star (where y_train == 0)
+non_planet_indices = np.where(y_train == 0)[0]
+first_non_planet_idx = non_planet_indices[0]
+
+# plot the first non-planet star
+plt.figure(figsize=(14, 4))
+plt.plot(X_train[first_non_planet_idx], color='crimson', lw=0.5)
+plt.title(f"Non-Planet/False Positive light curve - Star index {first_non_planet_idx}", fontsize=14)
+plt.xlabel("Time(sequential observations)", fontsize=12)
+plt.ylabel("Normalized flux", fontsize=12)
+plt.grid(True, alpha=0.3)
+plt.show()
